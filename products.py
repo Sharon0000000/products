e882498e003dde2bff442cd9078a3f332e949895
sharon0000000 <sharon8662229@gmail.com>
2023-05-22 21:31:56 +0800

    products.py

--- products.py
+++ products.py
@@ -1,16 +1,42 @@
-products = []
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q':
-    	break
-    price = input('請輸入商品價格:')
-    price = int(price)
-    products.append([name, price])
-print(products)
+import os
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
+         for line in f:
+    	     if '商品,價格' in line:
+    		     continue
+    		name, price = line.strip().split(',')
+    		products.append([name, price])
+    return products
 
-for p in products:
+        
+def user_input():
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+    	 break
+        price = input('請輸入商品價格:')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
+def print_products():
+    for p in products:
 	print(p[0], '的價格是', p[1]) 
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def write_file(filename, products):
+def main():
+	filename = 'products.csv'
+    if os.path.isfile(filename):
+	    print('yeah,有檔案')
+	    products = read_file('filename')
+    else:
+        print('找不到檔案')
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+main()
